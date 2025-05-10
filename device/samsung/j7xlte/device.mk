9ae68b9e1ef238656314372fdec192d97b079db5
bobthebuilder-69 <s.jasvndr@gmail.com>
2025-05-11 00:38:51 +0530

    Add files via upload

---
+++ device/samsung/j7xlte/device.mk
@@ -0,0 +1,6 @@
+PRODUCT_COPY_FILES += \
+    device/samsung/j7xlte/recovery.fstab:recovery/root/etc/recovery.fstab
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.model=SM-J710FN \
+    ro.product.device=j7xlte
